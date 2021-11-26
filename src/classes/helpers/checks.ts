a8e6f70d7ea5a2ccfe835b87ea9c0bce16642b28
Raphael Badawi <contact@raphaelbadawi.fr>
2021-11-26 16:14:02 +0100

    handle checks moves

--- src/classes/helpers/checks.ts
+++ src/classes/helpers/checks.ts
@@ -1,5 +1,37 @@
+import type { ChecksCell } from "../../types/types";
 import { Helper } from "./helper";
 
 export class ChecksHelper extends Helper {
-    
+    private traverseDiagonal(map: ChecksCell[], cell: ChecksCell, deltaX: number, deltaY: number): ChecksCell {
+        let originalCell: ChecksCell = cell;
+        while (!cell.hasPawn) {
+            cell = map.find(({ posX, posY }) => posX === cell.posX + deltaX && posY === cell.posY + deltaY);
+            if (!cell) return originalCell;
+        }
+        cell = map.find(({ posX, posY }) => posX === cell.posX + deltaX && posY === cell.posY + deltaY);
+        if (!cell || cell.hasPawn) return originalCell;
+        return cell;
+    }
+
+    /** Get available moves from an instance of the checks map */
+    override getAvailableMoves(map: ChecksCell[], startPoint: ChecksCell): ChecksCell[] {
+        const {
+            posX: currentPosX,
+            posY: currentPosY,
+        } = startPoint;
+        map = map.filter((cell) => {
+            if (
+                (cell.posX == currentPosX - 1 && cell.posY == currentPosY - 1)
+                || (cell.posX == currentPosX - 1 && cell.posY == currentPosY + 1)
+                || (cell.posX == currentPosX + 1 && cell.posY == currentPosY - 1)
+                || (cell.posX == currentPosX + 1 && cell.posY == currentPosY + 1)
+            ) {
+                if (cell.hasPawn) return false;
+                const deltaX = Math.sign(cell.posX - currentPosX);
+                const deltaY = Math.sign(cell.posY - currentPosY);
+                return this.traverseDiagonal(map, cell, deltaX, deltaY);
+            }
+        });
+        return map;
+    }
 }
--- src/classes/renderers/checks.ts
+++ src/classes/renderers/checks.ts
@@ -1,18 +1,19 @@
 import { PawnColor } from "../../enums/enums";
 import type { ChecksCell } from "../../types/types";
+import { ChecksHelper } from "../helpers/checks";
 import { Renderer } from "./renderer";
 
 export class ChecksRenderer extends Renderer {
+    override map: ChecksCell[];
     playerColor: PawnColor;
+    dragged: HTMLDivElement;
+    helper: ChecksHelper;
 
     constructor(map: ChecksCell[], playerColor: PawnColor) {
         super(map);
         this.playerColor = playerColor;
+        this.helper = new ChecksHelper();
     }
-
-    handlePlayerDrag = (event: DragEvent) => {
-        
-    };
 
     addPawn(cellElement: HTMLDivElement, color: PawnColor): HTMLDivElement {
         const pawnElement = document.createElement("div");
@@ -20,14 +21,35 @@ export class ChecksRenderer extends Renderer {
         if (color === this.playerColor) {
             pawnElement.classList.add("cursor-pointer");
             pawnElement.setAttribute("draggable", "true");
-            pawnElement.addEventListener("drag", this.handlePlayerDrag);
+            pawnElement.addEventListener("dragstart", e => {
+                this.dragged = e.target as HTMLDivElement;
+                this.dragged.style.opacity = "0.5";
+            });
         }
         return pawnElement;
     }
 
     override renderCell(cell: ChecksCell): HTMLDivElement {
         let cellElement: HTMLDivElement = super.renderCell(cell);
-        const { color } = cell;
+        cellElement.addEventListener("dragenter", e => {
+            const target = e.target as HTMLDivElement;
+            target.style.filter = "drop-shadow(16px 16px 20px red) invert(75%)";
+        });
+        cellElement.addEventListener("dragleave", e => {
+            const target = e.target as HTMLDivElement;
+            target.style.filter = "";
+        });
+        cellElement.addEventListener("dragover", e => e.preventDefault());
+        cellElement.addEventListener("drop", e => {
+            const target = e.target as HTMLDivElement;
+            target.style.filter = "";
+            this.dragged.style.opacity = "1";
+            const draggedCell = this.map.find(cell => cell.id == parseInt(this.dragged.parentElement.id.split("-")[1]));
+            const targetCell = this.map.find(cell => cell.id == parseInt(target.id.split("-")[1]));
+            if (!draggedCell || !targetCell) return;
+            const availableMoves = this.helper.getAvailableMoves(this.map, draggedCell);
+            if (availableMoves.includes(targetCell)) target.appendChild(this.dragged);
+        });
         if (cell.color == PawnColor.WHITE) cellElement.classList.add("white");
         if (cell.color == PawnColor.BLACK) cellElement.classList.add("black");
         if (cell.hasPawn) cellElement.appendChild(this.addPawn(cellElement, cell.hasPawn));
